ff8a6bea8abbc7b89c4ef0823ed92b086d69e4ee
utopiaeh <utopiaeh01@gmail.com>
2025-06-28 12:55:37 +0300

    changed path

--- hosts/darwin/flow48/default.nix
+++ hosts/darwin/flow48/default.nix
@@ -5,10 +5,10 @@
   ];
 
 sops = {
-    defaultSopsFile = ../../secrets/flow48/secrets.enc.yaml;
+    defaultSopsFile = ../../../secrets/flow48/secrets.enc.yaml;
   secrets = {
     "ssh/id_ed25519" = {
-    sopsFile =../../secrets/flow48/secrets.enc.yaml;
+    sopsFile = ../../../secrets/flow48/secrets.enc.yaml;
       path = "/Users/utopiaeh/.ssh/id_ed25519";
       mode = "0600";
       owner = "utopiaeh";
