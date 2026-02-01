8516b85a83456c73ac84cc3636708228ace0f7ca
utopiaeh <utopiaeh01@gmail.com>
2026-02-01 15:44:32 +0200

    new test

--- hosts/common/common-packages.nix
+++ hosts/common/common-packages.nix
@@ -2,16 +2,7 @@
 { inputs, pkgs, ... }:
 {
   nixpkgs.config.allowUnfree = true;
-  nix-darwin.extraConfig = ''
-    environment.variables = {
-      CARGO_HOME = "$HOME/.cargo";
-      RUSTUP_HOME = "$HOME/.rustup";
-      PATH = "$HOME/.cargo/bin:$PATH";
-      RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}/lib/rustlib/src/rust/library";
-      RA_LAUNCHER_CARGO_ALL_FEATURES = "true";
-      RA_LAUNCHER_PROC_MACRO_ENABLE = "true";
-    };
-  '';
+
   environment.systemPackages = with pkgs; [
     gh
     git-crypt
--- hosts/common/darwin/rust/default.nix
+++ hosts/common/darwin/rust/default.nix
@@ -1,44 +1,9 @@
-{ inputs, pkgs, ... }:
-
 let
-  inherit (inputs) nixpkgs;
+  # Pinned nixpkgs, deterministic. Last updated: 2/12/21.
+  pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/a58a0b5098f0c2a389ee70eb69422a052982d990.tar.gz")) {};
 
-  # Optional wrapper for LLVM coverage if you need it
-  cargoLlvmCovWrapped = pkgs.writeShellScriptBin "cargo-llvm-cov" ''
-    export LLVM_COV=${pkgs.rustc.llvmPackages.llvm}/bin/llvm-cov
-    export LLVM_PROFDATA=${pkgs.rustc.llvmPackages.llvm}/bin/llvm-profdata
-    exec ${pkgs.cargo-llvm-cov}/bin/cargo-llvm-cov "$@"
-  '';
-in
-{
-  # Global environment variables for all shells/IDEs
-  environment.variables = {
-    CARGO_HOME = "$HOME/.cargo";
-    RUSTUP_HOME = "$HOME/.rustup";
-    PATH = "$HOME/.cargo/bin:$PATH";
-    RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}/lib/rustlib/src/rust/library";
-
-    # Rust Analyzer flags for deep analysis
-    RA_LAUNCHER_CARGO_ALL_FEATURES = "true";
-    RA_LAUNCHER_PROC_MACRO_ENABLE = "true";
-  };
-
-  # Rust tooling installed via nix
-  environment.systemPackages = with pkgs; [
-    rustc
-    cargo
-    rustfmt
-    clippy
-    rust-analyzer
-    rustPlatform.rustLibSrc
-    cargoLlvmCovWrapped
-  ];
-
-  # Post-activation: confirm Rust installation without rustup
-  system.activationScripts.postActivation.text = ''
-    echo "❯ Rust toolchain is managed via Nix."
-    rustc --version
-    cargo --version
-    rust-analyzer --version
-  '';
+  # Rolling updates, not deterministic.
+  # pkgs = import (fetchTarball("channel:nixpkgs-unstable")) {};
+in pkgs.mkShell {
+  buildInputs = [ pkgs.cargo pkgs.rustc ];
 }
