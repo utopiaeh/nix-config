62c43a8d361dc4c3760110dd841286fd6fa2e111
utopiaeh <utopiaeh01@gmail.com>
2026-02-02 22:50:30 +0200

    add llvm-tools

--- hosts/common/darwin/rust/default.nix
+++ hosts/common/darwin/rust/default.nix
@@ -4,12 +4,13 @@
   # Rust toolchain (stable, latest)
   environment.systemPackages = [
     (pkgs.rust-bin.stable.latest.default.override {
-      extensions = [ "rustfmt" "clippy" "rust-src" ];
+      extensions = [ "rust-src" "rustfmt" "clippy" "llvm-tools" ];
     })
 
     pkgs.rust-analyzer
     pkgs.pkg-config
     pkgs.openssl
+    pkgs.cargo-llvm-cov
   ];
 
   # Ensure Cargo binaries are on PATH (usually already true)
